92c9aec5c704fea1c36e26068bcdba7e72c3908d
ManoharJunga <95352064+ManoharJunga@users.noreply.github.com>
2024-06-03 09:02:44 +0530

    commit

--- app/index.tsx
+++ app/index.tsx
@@ -8,42 +8,42 @@ import TabNavigation from './Navigations/TabNavigation'
 import Login from './Screens/LoginScreen/Login';
 
 const tokenCache = {
-  async getToken(key) {
-    try {
-      return SecureStore.getItemAsync(key);
-    } catch (err) {
-      return null;
-    }
-  },
-  async saveToken(key, value) {
-    try {
-      return SecureStore.setItemAsync(key, value);
-    } catch (err) {
-      return;
-    }
-  },
+    async getToken(key: string) {
+        try {
+            return SecureStore.getItemAsync(key);
+        } catch (err) {
+            return null;
+        }
+    },
+    async saveToken(key: string, value: string) {
+        try {
+            return SecureStore.setItemAsync(key, value);
+        } catch (err) {
+            return;
+        }
+    },
 };
 const index = () => {
-  return (
-    <ClerkProvider
-      tokenCache={tokenCache}
-      publishableKey='pk_test_dGVhY2hpbmctZmlsbHktOTguY2xlcmsuYWNjb3VudHMuZGV2JA'>
-      <View >
-        {/* Sign In Component */}
-        <SignedIn>
-          <NavigationContainer>
-            <TabNavigation />
-          </NavigationContainer>
-        </SignedIn>
-        {/* SignOut */}
-        <SignedOut>
-          <Login />
-        </SignedOut>
-        <StatusBar style="auto" />
-      </View>
-    </ClerkProvider>
+    return (
+        <ClerkProvider
+            tokenCache={tokenCache}
+            publishableKey='pk_test_dGVhY2hpbmctZmlsbHktOTguY2xlcmsuYWNjb3VudHMuZGV2JA'>
+            <View >
+                {/* Sign In Component */}
+                <SignedIn>
+                    <NavigationContainer>
+                        <TabNavigation />
+                    </NavigationContainer>
+                </SignedIn>
+                {/* SignOut */}
+                <SignedOut>
+                    <Login />
+                </SignedOut>
+                <StatusBar style="auto" />
+            </View>
+        </ClerkProvider>
 
-  )
+    )
 }
 
 export default index
